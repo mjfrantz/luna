7be0d768f0245913e9a7492a56b1cbb20db0762f
Ivan <ivanvlora@gmail.com>
2020-06-08 10:33:40 +0900

    added contentful to app

--- src/pages/products.js
+++ src/pages/products.js
@@ -1,30 +1,45 @@
 import { useState } from 'react';
+import client from '../../config/contentful';
+
 import { Row, Col, Layout, Typography, Card, Avatar, Tag, Button } from 'antd';
 
 const { Content } = Layout;
 const { Title, Subtitle, Text } = Typography;
 const { Meta } = Card;
 
-const Product = () => {
-  const createProduct = (count) => {
-    const products = [];
-    const arr = Array.from(Array(count).keys());
+export async function getStaticProps() {
+  const entries = await client.getEntries();
 
-    for (const item in arr) {
-      products.push({
-        title: `Product ${+item + 1}`,
-        description: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laborum nulla recusandae praesentiun.',
-        quantity: Math.ceil(Math.random() * 10000),
-        price: "$" + Math.round(Math.random() * 100).toFixed(2)
-      });
-    }
+  if (entries.items) return { props: { entries } };
 
-    return products;
-  };
+  console.log(`Error getting Entries for ${contentType.name}.`);
+}
 
-  const [products, setProducts] = useState(createProduct(2));
+const Product = ({ entries }) => {
+  const products = entries.items;
 
-  console.log(createProduct(5));
+  console.log(products);
+
+  // const createProduct = (count) => {
+  //   const products = [];
+  //   const arr = Array.from(Array(count).keys());
+
+  //   for (const item in arr) {
+  //     products.push({
+  //       title: `Product ${+item + 1}`,
+  //       description:
+  //         'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laborum nulla recusandae praesentiun.',
+  //       quantity: Math.ceil(Math.random() * 10000),
+  //       price: '$' + Math.round(Math.random() * 100).toFixed(2),
+  //     });
+  //   }
+
+  //   return products;
+  // };
+
+  // const [products, setProducts] = useState(createProduct(2));
+
+  // console.log(createProduct(5));
 
   return (
     <div>
@@ -36,29 +51,46 @@ const Product = () => {
           optio?
         </Title>
 
-        <Row justify="center" gutter={[42, 42]} style={{ marginTop: '5rem', padding: '0 4rem' }}>
-          {products.map((product) => (
-            <Col xs={24} sm={24} md={10} lg={8} xl={6}>
-              <Card
-                cover={
-                  <img
-                    alt='example'
-                    src='https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png'
-                  />
-                }
-                actions={[
-                  <Tag>{product.price}</Tag>,
-                  <Button type="primary" small>Buy Now</Button>,
-                ]}
+        {products.entries.length ? (
+          <Row
+            justify='center'
+            gutter={[42, 42]}
+            style={{ marginTop: '5rem', padding: '0 4rem' }}
+          >
+            {products.map((product) => (
+              <Col
+                xs={24}
+                sm={24}
+                md={10}
+                lg={8}
+                xl={6}
+                key={product.fields.title}
               >
-                <Meta
-                  title={product.title}
-                  description={product.description}
-                />
-              </Card>
-            </Col>
-          ))}
-        </Row>
+                <Card
+                  cover={
+                    <img
+                      alt='example'
+                      src={product.fields.image.fields.file.url}
+                    />
+                  }
+                  actions={[
+                    <Tag>{product.fields.price}</Tag>,
+                    <Button type='primary' small>
+                      Buy Now
+                    </Button>,
+                  ]}
+                >
+                  <Meta
+                    title={product.fields.title}
+                    description={product.fields.description}
+                  />
+                </Card>
+              </Col>
+            ))}
+          </Row>
+        ) : (
+          <Title>There are no products to display</Title>
+        )}
       </Content>
     </div>
   );
