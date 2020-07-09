c4c97a6700d513f2ad5c9dde521bb13c8204c29d
Michael Frantz <mjfrantz26@gmail.com>
2020-07-09 12:41:11 -0500

    Added links

--- src/components/layout/Product.js
+++ src/components/layout/Product.js
@@ -1,4 +1,6 @@
-import { Row, Col, Layout, Typography, Card } from 'antd';
+import { Row, Col, Layout, Typography, Card, Button } from 'antd';
+import Link from 'next/link';
+
 
 
 const { Content } = Layout;
@@ -12,18 +14,20 @@ const Product = ({ data }) => {
       <Content style={{ paddingTop: '1rem' }}>
         <Title level={5} style={{ textAlign: 'center' }}>#1 rated customized hair care</Title>
         <p style={{ textAlign: 'center' }}>Your hair type. Your hair goals. Your color. Your fragrance. You name it (literally).</p>
-        <Row justify="space-around" style={{ margin: '1rem' }}>
-          {data.items.map(product => (
-            <Card
-              hoverable
-              style={{ width: 240 }}
-              cover={<img alt="example" src={product.fields.image && product.fields.image.fields.file.url} />}
-            >
-              <Meta title={product.fields.title}
-                description={product.fields.description} style={{ textAlign: 'center' }} />
-            </Card>
-          ))}
-        </Row>
+        <Link href="/products">
+          <Row justify="space-around" style={{ margin: '1rem' }}>
+            {data.items.map(product => (
+              <Card
+                hoverable
+                style={{ width: 240 }}
+                cover={<img alt="example" src={product.fields.image && product.fields.image.fields.file.url} />}
+              >
+                <Meta title={product.fields.title}
+                  description={product.fields.description} style={{ textAlign: 'center' }} />
+              </Card>
+            ))}
+          </Row>
+        </Link>
       </Content>
 
     </div >
